2349b9e7f2feb00381c89d52a82e0a8ad40ecb71
mikezalik <mike.zalik@gmail.com>
2018-11-26 19:57:35 -0800

    comments

--- guessGame.java
+++ guessGame.java
@@ -3,25 +3,30 @@ public class GuessGame {
     Player p2;
     Player p3;
 
+//player objects
     public void startGame() {
         p1 = new Player();
         p2 = new Player();
         p3 = new Player();
 
+//declare guess variables
         int guessp1 = 0;
         int guessp2 = 0;
         int guessp3 = 0;
 
+//declare true or false based on response
         boolean p1isRight = false;
         boolean p2isRight = false;
         boolean p3isRight = false;
 
+//make target number
         int targetNumber = (int) (Math.random()*10);
         System.out.println("I'm thinking of a number between 0 and 9...");
 
         while(true) {
             System.out.println("Number to guess is " + targetNumber);
-            
+
+//call guess methods
             p1.guess();
             p2.guess();
             p3.guess();
@@ -44,15 +49,16 @@ public class GuessGame {
             if (guessp3 == targetNumber) {
                 p3isRight = true;
             }
-            if (p1isRight || p2isRight || p3isRight){
+            if (p1isRight || p2isRight || p3isRight) {
                 System.out.println("We have a winner!");
                 System.out.println("Player one got it right? " + p1isRight);
                 System.out.println("Player two got it right? " + p2isRight);
                 System.out.println("Player three got it right? " + p3isRight);
                 System.out.println("Game is over.");
                 break;
-            }
-        }
-
-    }
-}
+            } else {
+                System.out.println("Players will have to try again.");
+            } //end if else
+        } //end loop
+    } //end method
+} //end class
